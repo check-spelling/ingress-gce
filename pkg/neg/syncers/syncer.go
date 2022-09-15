ab6e68655fc969235ab8f9528852b91bd3d9aa9d
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-09-15 17:02:59 -0400

    spelling: syncer
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/neg/syncers/syncer.go
+++ pkg/neg/syncers/syncer.go
@@ -81,7 +81,7 @@ func (s *syncer) Start() error {
 		return fmt.Errorf("NEG syncer for %s is shutting down. ", s.NegSyncerKey.String())
 	}
 
-	s.logger.V(2).Info("Starting NEG syncer for service port", "negSynckerKey", s.NegSyncerKey.String())
+	s.logger.V(2).Info("Starting NEG syncer for service port", "negSyncerKey", s.NegSyncerKey.String())
 	s.init()
 	go func() {
 		for {
@@ -111,7 +111,7 @@ func (s *syncer) Start() error {
 					s.stateLock.Lock()
 					s.shuttingDown = false
 					s.stateLock.Unlock()
-					s.logger.V(2).Info("Stopping NEG syncer", "negSynckerKey", s.NegSyncerKey.String())
+					s.logger.V(2).Info("Stopping NEG syncer", "negSyncerKey", s.NegSyncerKey.String())
 					return
 				}
 			case <-retryCh:
@@ -133,7 +133,7 @@ func (s *syncer) Stop() {
 	s.stateLock.Lock()
 	defer s.stateLock.Unlock()
 	if !s.stopped {
-		s.logger.V(2).Info("Stopping NEG syncer for service port", "negSynckerKey", s.NegSyncerKey.String())
+		s.logger.V(2).Info("Stopping NEG syncer for service port", "negSyncerKey", s.NegSyncerKey.String())
 		s.stopped = true
 		s.shuttingDown = true
 		close(s.syncCh)
@@ -142,7 +142,7 @@ func (s *syncer) Stop() {
 
 func (s *syncer) Sync() bool {
 	if s.IsStopped() {
-		s.logger.Info("NEG syncer is already stopped.", "negSynckerKey", s.NegSyncerKey.String())
+		s.logger.Info("NEG syncer is already stopped.", "negSyncerKey", s.NegSyncerKey.String())
 		return false
 	}
 	select {
